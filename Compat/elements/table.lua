4aa1fba5a9ff543630a24a2fa1c31042a92e39a9
Kader B <bkader@mail.com>
2021-11-16 13:07:21 +0100

    table: added Table class & TablePool functions
    - Table class has 2 functions: "get" to fetch a table or create it, and "free" to simply erases and parks the table.
    - TablePool creates a table pool system, returning "new" and "del" function to be used to recycle tables.

--- Compat/elements/table.lua
+++ Compat/elements/table.lua
@@ -80,21 +80,105 @@ function Compat.WeakTable(t)
 	return setmetatable(wipe(t or {}), weaktable)
 end
 
-local tablePool = ns.tablePool or setmetatable({}, {__mode = "k"})
-ns.tablePool = tablePool
+do
+	local Table = {}
+	local max_pool_size = 200
+	local pools = {}
 
-function Compat.NewTable()
-	local t = next(tablePool) or {}
-	tablePool[t] = nil
-	return t
+	-- attempts to get a table from the table pool of the
+	-- specified tag name. if the pool doesn't exist or is empty
+	-- it creates a lua table.
+	function Table.get(tag)
+		local pool = pools[tag]
+		if not pool then
+			pool = {}
+			pools[tag] = pool
+			pool.c = 0
+			pool[0] = 0
+		else
+			local len = pool[0]
+			if len > 0 then
+				local obj = pool[len]
+				pool[len] = nil
+				pool[0] = len - 1
+				return obj
+			end
+		end
+		return {}
+	end
+
+	-- releases the already used lua table into the table pool
+	-- named "tag" or creates it right away.
+	function Table.free(tag, obj, noclear)
+		if not obj then return end
+
+		local pool = pools[tag]
+		if not pool then
+			pool = {}
+			pools[tag] = pool
+			pool.c = 0
+			pool[0] = 0
+		end
+
+		if not noclear then
+			setmetatable(obj, nil)
+			for k, _ in pairs(obj) do
+				obj[k] = nil
+			end
+		end
+
+		do
+			local cnt = pool.c + 1
+			if cnt >= 20000 then
+				pool = {}
+				pools[tag] = pool
+				pool.c = 0
+				pool[0] = 0
+				return
+			end
+			pool.c = cnt
+		end
+
+		local len = pool[0] + 1
+		if len > max_pool_size then
+			return
+		end
+
+		pool[len] = obj
+		pool[0] = len
+	end
+
+	Compat.Table = Table
 end
 
-function Compat.DelTable(t)
-	if type(t) == "table" then
-		wipe(t)
-		t[true] = true
-		t[true] = nil
-		tablePool[t] = true
+-- Table Pool for recycling tables
+-- creates a new table system that can be used to reuse tables
+-- it returns both "new" and "del" functions.
+function Compat.TablePool()
+	local pool = {}
+	setmetatable(pool, {__mode = "k"})
+
+	-- attempts to retrieve a table from the cache
+	-- creates if if it doesn't exist.
+	local new = function()
+		local t = next(pool) or {}
+		pool[t] = nil
+		return t
 	end
-	return nil
+
+	-- it will wipe the provided table then cache it
+	-- to be reusable later.
+	local del = function(t)
+		if type(t) == "table" then
+			for k, _ in pairs(t) do
+				t[k] = nil
+			end
+			t[true] = true
+			t[true] = nil
+			pool[t] = true
+		end
+		return nil
+	end
+
+	return new, del
 end
