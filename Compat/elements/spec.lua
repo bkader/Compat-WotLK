dd3693e4227ba945bcff88e3f3932ce42700472c
Kader Bouyakoub <bkader@mail.com>
2022-03-16 00:39:57 +0100

    r30: added GetSpecializationInfoByID func
    To have localized specializations names, "LibBabble-TalentTree-3.0" had to be included.

--- Compat/elements/spec.lua
+++ Compat/elements/spec.lua
@@ -105,8 +105,173 @@ end
 
 -- aliases
 Compat.UnitGroupRolesAssigned = Compat.GetSpecializationRole
-Compat.GetUnitRole = Compat.GetSpecializationRole
 Compat.GetUnitSpec = Compat.GetInspectSpecialization
+Compat.GetUnitRole = Compat.GetSpecializationRole
+
+function Compat.GetSpecializationInfo(specIndex, isInspect, isPet, specGroup)
+	local name, icon, _, background = GetTalentTabInfo(specIndex, isInspect, isPet, specGroup)
+	local id, role
+	if isInspect and unitExists("target") then
+		id, role = Compat.GetInspectSpecialization("target"), Compat.GetSpecializationRole("target")
+	else
+		id, role = Compat.GetInspectSpecialization("player"), Compat.GetSpecializationRole("player")
+	end
+	return id, name, "NaN", icon, background, role
+end
+
+local LT = LibStub("LibBabble-TalentTree-3.0"):GetLookupTable()
+function Compat.GetSpecializationInfoByID(id)
+	local name, icon, class
+	local role = "DAMAGER"
+
+	-- DEATHKNIGHT --
+	if id == 250 then -- Blood
+		name = LT.Blood
+		icon = [[Interface\\Icons\\spell_deathknight_bloodpresence]]
+		class = "DEATHKNIGHT"
+	elseif id == 251 then -- Frost
+		name = LT.Frost
+		icon = [[Interface\\Icons\\spell_deathknight_frostpresence]]
+		class = "DEATHKNIGHT"
+	elseif id == 252 then -- Unholy
+		name = LT.Unholy
+		icon = [[Interface\\Icons\\spell_deathknight_unholypresence]]
+		class = "DEATHKNIGHT"
+	-- DRUID --
+	elseif id == 102 then -- Balance
+		name = LT.Balance
+		icon = [[Interface\\Icons\\spell_nature_starfall]]
+		class = "DRUID"
+	elseif id == 103 then -- Feral Combat (Damager)
+		name = LT["Feral Combat"]
+		icon = [[Interface\\Icons\\ability_druid_catform]]
+		class = "DRUID"
+	elseif id == 104 then -- Feral Combat (Tank)
+		name = LT["Feral Combat"]
+		icon = [[Interface\\Icons\\ability_racial_bearform]]
+		role = "TANK"
+		class = "DRUID"
+	elseif id == 105 then -- Restoration
+		name = LT.Restoration
+		icon = [[Interface\\Icons\\spell_nature_healingtouch]]
+		role = "HEALER"
+		class = "DRUID"
+	-- HUNTER --
+	elseif id == 253 then -- Beast Mastery
+		name = LT["Beast Mastery"]
+		icon = [[Interface\\Icons\\ability_hunter_beasttaming]]
+		class = "HUNTER"
+	elseif id == 254 then -- Marksmanship
+		name = LT.Marksmalship
+		icon = [[Interface\\Icons\\ability_hunter_focusedaim]]
+		role = "TANK"
+		class = "HUNTER"
+	elseif id == 255 then -- Survival
+		name = LT.Survival
+		icon = [[Interface\\Icons\\ability_hunter_swiftstrike]]
+		role = "HEALER"
+		class = "HUNTER"
+	-- MAGE --
+	elseif id == 62 then -- Arcane
+		name = LT.Arcane
+		icon = [[Interface\\Icons\\spell_holy_magicalsentry]]
+		class = "MAGE"
+	elseif id == 63 then -- Fire
+		name = LT.Fire
+		icon = [[Interface\\Icons\\spell_fire_flamebolt]]
+		class = "MAGE"
+	elseif id == 64 then -- Frost
+		name = LT.Frost
+		icon = [[Interface\\Icons\\spell_frost_frostbolt02]]
+		class = "MAGE"
+	-- PALADIN --
+	elseif id == 65 then -- Holy
+		name = LT.Holy
+		icon = [[Interface\\Icons\\spell_holy_holybolt]]
+		role = "HEALER"
+		class = "PALADIN"
+	elseif id == 66 then -- Protection
+		name = LT.Protection
+		icon = [[Interface\\Icons\\ability_paladin_shieldofthetemplar]]
+		role = "TANK"
+		class = "PALADIN"
+	elseif id == 70 then -- Retribution
+		name = LT.Retribution
+		icon = [[Interface\\Icons\\spell_holy_auraoflight]]
+		class = "PALADIN"
+	-- PRIEST --
+	elseif id == 256 then -- Discipline
+		name = LT.Discipline
+		icon = [[Interface\\Icons\\spell_holy_holybolt]]
+		role = "HEALER"
+		class = "PRIEST"
+	elseif id == 257 then -- Holy
+		name = LT.Holy
+		icon = [[Interface\\Icons\\ability_paladin_shieldofthetemplar]]
+		role = "HEALER"
+		class = "PRIEST"
+	elseif id == 258 then -- Shadow
+		name = LT.Shadow
+		icon = [[Interface\\Icons\\spell_holy_auraoflight]]
+		class = "PRIEST"
+	-- ROGUE --
+	elseif id == 259 then -- Assassination
+		name = LT.Assassination
+		icon = [[Interface\\Icons\\ability_rogue_eviscerate]]
+		class = "ROGUE"
+	elseif id == 260 then -- Combat
+		name = LT.Combat
+		icon = [[Interface\\Icons\\ability_backstab]]
+		class = "ROGUE"
+	elseif id == 261 then -- Subtlety
+		name = LT.Subtlety
+		icon = [[Interface\\Icons\\ability_stealth]]
+		class = "ROGUE"
+	-- SHAMAN --
+	elseif id == 262 then -- Elemental
+		name = LT.Elemental
+		icon = [[Interface\\Icons\\spell_nature_lightning]]
+		class = "SHAMAN"
+	elseif id == 263 then -- Enhancement
+		name = LT.Enhancement
+		icon = [[Interface\\Icons\\spell_shaman_improvedstormstrike]]
+		class = "SHAMAN"
+	elseif id == 264 then -- Restoration
+		name = LT.Restoration
+		icon = [[Interface\\Icons\\spell_nature_healingwavegreater]]
+		role = "HEALER"
+		class = "SHAMAN"
+	-- WARLOCK --
+	elseif id == 265 then -- Affliction
+		name = LT.Affliction
+		icon = [[Interface\\Icons\\spell_shadow_deathcoil]]
+		class = "WARLOCK"
+	elseif id == 266 then -- Demonology
+		name = LT.Demonology
+		icon = [[Interface\\Icons\\spell_shadow_metamorphosis]]
+		class = "WARLOCK"
+	elseif id == 267 then -- Destruction
+		name = LT.Destruction
+		icon = [[Interface\\Icons\\spell_shadow_rainoffire]]
+		class = "WARLOCK"
+	-- WARRIOR --
+	elseif id == 71 then -- Arms
+		name = LT.Arms
+		icon = [[Interface\\Icons\\ability_warrior_savageblow]]
+		class = "WARRIOR"
+	elseif id == 72 then -- Fury
+		name = LT.Fury
+		icon = [[Interface\\Icons\\ability_warrior_innerrage]]
+		class = "WARRIOR"
+	elseif id == 73 then -- Protection
+		name = LT.Protection
+		icon = [[Interface\\Icons\\ability_warrior_defensivestance]]
+		role = "TANK"
+		class = "WARRIOR"
+	end
+
+	return id, name, "NaN", icon, nil, role, class
+end
 
 -- utilities
 
--- Compat/init.lua
+++ Compat/init.lua
@@ -1,2 +1,2 @@
 local parent, ns = ...
-ns.Compat = {ver = 1, rev = 29, Private = {}}
+ns.Compat = {ver = 1, rev = 30, Private = {}}
